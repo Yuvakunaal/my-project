64cdb8949aa16b865a743c286bc66300eeee94f2
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-02 05:58:17 +0000

    message5

--- Day-8/leet.py
+++ Day-8/leet.py
@@ -67,5 +67,20 @@ class MinStack:
             return None
         return self.min_stack[-1]
 
+# https://leetcode.com/problems/remove-duplicate-letters/description/
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        last_occurrence = {char: i for i, char in enumerate(s)}
+        print(last_occurrence)
+        seen = set()
+        for i, char in enumerate(s):
+            if char not in seen:
+                while (stack and char < stack[-1] and i < last_occurrence[stack[-1]]):
+                    seen.remove(stack.pop())
+                stack.append(char)
+                seen.add(char)
+            print(stack)
+        return "".join(stack)
 
         
