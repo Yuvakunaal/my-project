22c1fc9b3a47b5f2870b43fff533322e8219bcaf
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-02 07:24:31 +0000

    message1

--- Day-8/leet.py
+++ Day-8/leet.py
@@ -98,6 +98,33 @@ class Solution:
             s.append(i)
         return False
 
+# https://leetcode.com/problems/valid-parenthesis-string/description/
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        min_open = 0
+        max_open = 0
+        
+        for char in s:
+            if char == '(':
+                min_open += 1
+                max_open += 1
+
+            elif char == ')':
+                if min_open > 0:
+                    min_open -= 1
+                max_open -= 1
+
+            elif char == '*':
+                if min_open > 0:
+                    min_open -= 1
+                max_open += 1
+
+            if max_open < 0:
+                return False
+                
+        return min_open == 0
+
+
 
 
         
