0f98f61f8511e55e42ecb1119d67545618b71cb7
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-02 03:52:42 +0000

    message1

---
+++ Day-8/1.py
@@ -0,0 +1,48 @@
+class Stack:
+    def __init__(self, size):
+        self.size = size
+        self.stack = []
+
+    def push(self, element):
+        if len(self.stack) == self.size:
+            print("Stack overflow")
+        else:
+            self.stack.append(element)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            print("Stack underflow")
+        else:
+            return self.stack.pop()
+
+    def peek(self):
+        if len(self.stack) == 0:
+            print("Stack is empty")
+        else:
+            return self.stack[-1]
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def isFull(self):
+        return len(self.stack) == self.size
+
+    def show_size(self):
+        return len(self.stack)
+
+    def display(self):
+        return self.stack
+
+postfix = input("Enter valid postfix : ").strip()
+s = Stack(len(postfix))
+p = {"+": 1, "-": 1, "*": 2, "/": 2, "%": 2}
+for i in postfix:
+    if i in p:
+        op1 = s.pop()
+        op2 = s.pop()
+        a = i + op2 + op1 
+        s.push(a)
+    else: 
+        s.push(i)
+prefix = s.pop()
+print(prefix)
