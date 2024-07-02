164aa88616d9ac0fb81c8f84e60a583bebf75b99
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-02 05:14:28 +0000

    message4

--- Day-8/leet.py
+++ Day-8/leet.py
@@ -1,5 +1,4 @@
 # https://leetcode.com/problems/simplify-path/description/
-
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
@@ -12,7 +11,32 @@ class Solution:
                 stack.append(part)
         simplified_path = '/' + '/'.join(stack)
         return simplified_path
-s = Solution()
-print(s.simplifyPath("/home/user/Documents/../Pictures"))
+
+# https://leetcode.com/problems/min-stack/description/
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        return None
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        return None
+
+    def pop(self) -> None:
+        if len(self.stack) == 0:
+            return None
+        self.stack.pop()
+        return None
+
+    def top(self) -> int:
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if len(self.stack) == 0:
+            return None
+        return min(self.stack)
+
 
         
