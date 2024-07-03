25694ba333fea58a972c73cf3ca5e9379fa8acb3
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-03 04:38:26 +0000

    message4

--- Day-9/leet.py
+++ Day-9/leet.py
@@ -19,4 +19,8 @@ class Solution:
         for i in range(1,len(gain)):
             gain[i]+=gain[i-1]
         return max(gain)
-        
+
+# https://leetcode.com/problems/find-the-highest-altitude/description/
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        return sum([sum(arr[i:j+1]) for i in range(len(arr)) for j in range(i, len(arr)) if (j - i + 1) % 2 != 0])
