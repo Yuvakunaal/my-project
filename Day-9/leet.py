dbdf7e8d7ed86a37d2a227b18a536861e28a1770
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-03 04:07:17 +0000

    message3

--- Day-9/leet.py
+++ Day-9/leet.py
@@ -4,3 +4,11 @@ class Solution:
         for i in range(1,len(nums)):
             nums[i]+=nums[i-1]
         return nums
+
+# https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/description/
+class Solution:
+    def differenceOfSum(self, nums: List[int]) -> int:
+        a = sum(nums)
+        b = sum([int(digit) for num in nums for digit in str(num)])
+        return abs(a-b)
+
