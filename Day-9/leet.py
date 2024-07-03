4bcd09edce3970613b6c53d7bca1da1f8e613dc7
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-03 05:12:07 +0000

    message4

--- Day-9/leet.py
+++ Day-9/leet.py
@@ -24,3 +24,15 @@ class Solution:
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         return sum([sum(arr[i:j+1]) for i in range(len(arr)) for j in range(i, len(arr)) if (j - i + 1) % 2 != 0])
+
+# https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        return (sorted(nums)[-2]-1)*(sorted(nums)[-1]-1)
+
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        num = set(nums)
+        return [i for i in range(1,len(nums)+1) if i not in num]
+    
