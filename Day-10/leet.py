e1c357c7a6bca98d7ceffda6f24e492f8aef8893
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-04 04:08:44 +0000

    m1

--- Day-10/leet.py
+++ Day-10/leet.py
@@ -9,3 +9,9 @@ class Solution:
             elif len(current_string) > len(s):
                 return False
         return False
+
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        gcd_len = lambda a, b: a if b == 0 else gcd_len(b, a % b)
+        return str1[:gcd_len(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ""
