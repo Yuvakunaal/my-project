172ab33564bef5711db5838c4e1fefbec5708af3
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-05 07:31:04 +0000

    m10

--- Day-11/leet.py
+++ Day-11/leet.py
@@ -11,3 +11,12 @@ class Solution:
             if a == b:
                 return True
         return False
+
+# https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        l1,l2 = headA,headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
