e7640830de7ff3b0efaaeb4c4bdb83aa47a947ab
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-09 04:47:14 +0000

    m1

--- DSA/SelectionSort.py
+++ DSA/SelectionSort.py
@@ -1,3 +1,14 @@
 def SelectionSort(arr):
-    #
-    return
+    N = len(arr)
+    for i in range(0,N-1):  
+        min_index = i 
+        for j in range(i+1,N):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index != i:  
+            arr[i],arr[min_index] = arr[min_index],arr[i]
+def run():
+    arr = [80,45,60,40,30,10,67,88,23]
+    SelectionSort(arr)
+    print(arr)
+run()
