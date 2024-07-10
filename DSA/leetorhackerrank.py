7c81cc746a2a598ca731a89616afa64b77a9bbfe
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-10 06:12:36 +0000

    m1

--- DSA/leetorhackerrank.py
+++ DSA/leetorhackerrank.py
@@ -121,4 +121,13 @@ class Solution:
         else: 
             return self.searchBST(root.left, val)
 
-# 
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right,val)
+        else:
+            root.left = self.insertIntoBST(root.left,val)
+        return root
