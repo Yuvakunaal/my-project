5259e7e4ae2b77437f6616ee2128c006b860ffda
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-12 07:36:46 +0000

    m

--- DSA/QuickSort.py
+++ DSA/QuickSort.py
@@ -12,7 +12,6 @@ def QuickSort(arr,left,right):
         pivot_index = partition(arr, left, right) #fix, find, sort the pivot
         QuickSort(arr,left,pivot_index-1) #sort left list 
         QuickSort(arr,pivot_index+1,right) #sort right list 
-
 def run():
     arr = [80,45,60,40,30,10,67,88,23]
     QuickSort(arr,0,len(arr)-1)
