3b62044d0b70aa36ce6b9c79479d5dc81743c057
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-12 06:36:46 +0000

    m1

--- DSA/Graphs.py
+++ DSA/Graphs.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Depth First Search (DFS) - Normal :-
 def DFS_N(graph, start):
     visited = set()  # all visited nodes will be stored here
@@ -22,8 +24,41 @@ def DFS_R(graph, node, visited=None):
         for neighbor in sorted(graph[node]):
             DFS_R(graph, neighbor, visited)
 
-# Breadth First Search (BFS) - Normal :-
-def BFS_N(graph):
+# Breadth First Search (BFS) - Iterative :-
+def BFS_N(graph, start):
+    visited = set()  # All visited nodes will be stored here
+    queue = [start]
+    visited.add(start)
+    while queue:
+        node = queue.pop(0)
+        print(node, end=" ")
+        neighbors = graph[node]
+        for neighbor in sorted(neighbors):  # Sort neighbors to ensure consistent order
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Breadth First Search (BFS) - Recursion :-
+def BFS_recursive(graph, queue, visited):
+    if not queue:
+        return
+
+    node = queue.popleft()
+    print(node, end=" ")
+
+    for neighbor in sorted(graph[node]):
+        if neighbor not in visited:
+            visited.add(neighbor)
+            queue.append(neighbor)
+    
+    BFS_recursive(graph, queue, visited)
+
+def BFS_R(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    BFS_recursive(graph, queue, visited)
+
 def run():
     graph = {
         "A": ["B", "C"],
@@ -37,5 +72,10 @@ def run():
     DFS_N(graph, "A")
     print("\nDFS - Recursion = ",end="")
     DFS_R(graph, "A")
+    print("\nBFS - Normal = ",end="")
+    BFS_N(graph, "A")
+    print("\nBFS - Recursion = ",end="")
+    BFS_R(graph, "A")
     print()
+    
 run()
