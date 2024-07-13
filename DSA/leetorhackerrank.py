9b27c255f2657cdb8d5ac9d46647d515938c6d3f
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-13 07:34:13 +0000

    m3

--- DSA/leetorhackerrank.py
+++ DSA/leetorhackerrank.py
@@ -321,4 +321,21 @@ class Solution:
         
         return num_islands
 
-
+# https://leetcode.com/problems/word-ladder/
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+        queue = deque([(beginWord,1)])
+        while queue: 
+            word, level = queue.popleft()
+            if word == endWord:
+                return level 
+            for i in range(len(word)):
+                for ch in 'abcdefghijklmnopqrstuvwxyz':
+                    new_word = word[:i] + ch + word[i+1:]
+                    if new_word in wordSet:
+                        wordSet.remove(new_word)
+                        queue.append((new_word,level+1))
+        return 0 
